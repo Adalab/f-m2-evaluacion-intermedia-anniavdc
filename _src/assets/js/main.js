b11ef08a6de454aa2b4c31493e82361104572ec8
anniavdc <anva1189@gmail.com>
2019-04-08 18:22:38 +0200

    Add js functions

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -15,23 +15,67 @@ contabilizando el número de intentos. Hasta que al final el jugador acierta el 
     //p azul con pistas
 
 //Recoger los elementos de HTML en JS
-    //label
     //input
     //button
     //p rojo
     //p azul
 
+const userNumber = document.querySelector('#number');
+const userAttempts = document.querySelector('.attempts');
+const userResult = document.querySelector('.result');
+const button = document.querySelector('.button');
+
+console.log(button);
+
+
 //Añadir un listener al button
 //console.log
+//button.addEventListener('click', handlerButton);
 
 //Funcion generadora de números al azar entre 0-100
+
+function getRandomNumber(min=0,max=100){
+    console.log('chao');
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+console.log(getRandomNumber());
+
 //event.preventDefault para el button
 //console.log
+
 //Función para que al pulsar el botón de prueba, acceder al contenido del input y mostrarlo en la consola
-//Función comparar el número que el usuario ha escrito en el input con el número aleatorio, y pintar el
-//console.log
-//feedback correspondiente en la pantalla ("demasiado alto", "demasiado bajo", "¡HAS
+
+function getUserNumber(event){
+    event.preventDefault();
+    console.log (userNumber.value);
+}
+
+//button.addEventListener('click', getUserNumber);
+
+//Función comparar el número que el usuario ha escrito en el input con el número aleatorio, y pintar el feedback correspondiente en la pantalla ("demasiado alto", "demasiado bajo", "¡HAS
 //GANADO, CAMPEONA!")
+
+
+function compareNumbers(event){
+    const input = parseInt(getUserNumber);
+    event.preventDefault();
+    console.log('hola');
+    if(input > getRandomNumber){
+        userResult.innerHTML = 'demasiado alto';
+    }else if(input < getRandomNumber){
+        userResult.innerHTML = 'demasiado bajo';
+    }else if(input === getRandomNumber){
+        userResult.innerHTML = 'Has ganado, campeona!';
+    }
+}
+
+button.addEventListener('click', compareNumbers);
+
+
 //console.log
 
 //actualizar el contador de intentos cada vez que el usuario pruebe
