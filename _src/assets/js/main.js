ecefc8c12fd3bb7f54e9000ec4535d5c59719946
anniavdc <anva1189@gmail.com>
2019-04-08 17:26:34 +0200

    Add written steps in js

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,3 +1,32 @@
 'use strict';
 
 console.log('>> Ready :)');
+
+/*El juego consiste en que el juego genera un número al azar entre 1 y 100, y el jugador tiene que
+adivinarlo. El juego le da pistas de si el número que prueba es demasiado alto o bajo, y va
+contabilizando el número de intentos. Hasta que al final el jugador acierta el número.*/
+
+//Crear HTML
+    //titulo
+    //label
+    //input
+    //button
+    //p rojo con intentos realizados
+    //p azul con pistas
+
+//Recoger los elementos de HTML en JS
+    //label
+    //input
+    //button
+    //p rojo
+    //p azul
+
+//Añadir un listener al button
+
+//Funcion generadora de números al azar entre 0-100
+//Función para que al pulsar el botón de prueba, acceder al contenido del input y mostrarlo en la consola
+//Función comparar el número que el usuario ha escrito en el input con el número aleatorio, y pintar el
+//feedback correspondiente en la pantalla ("demasiado alto", "demasiado bajo", "¡HAS
+//GANADO, CAMPEONA!")
+
+//actualizar el contador de intentos cada vez que el usuario pruebe
