84744b1ac1d9940de0d45f5762dcaa4fd729767d
anniavdc <anva1189@gmail.com>
2019-04-09 11:15:35 +0200

    Remove blank spaces and remaining comments

--- _src/assets/js/main.js
+++ _src/assets/js/main.js
@@ -1,25 +1,5 @@
 'use strict';
 
-console.log('>> Ready :)');
-
-/*El juego consiste en que el juego genera un número al azar entre 1 y 100, y el jugador tiene que
-adivinarlo. El juego le da pistas de si el número que prueba es demasiado alto o bajo, y va
-contabilizando el número de intentos. Hasta que al final el jugador acierta el número.*/
-
-//Crear HTML
-    //titulo
-    //label
-    //input
-    //button
-    //p rojo con intentos realizados
-    //p azul con pistas
-
-//Recoger los elementos de HTML en JS
-    //input
-    //button
-    //p rojo
-    //p azul
-
 const userNumber = document.querySelector('#number');
 const userAttempts = document.querySelector('.attempts');
 const userResult = document.querySelector('.result');
@@ -27,12 +7,6 @@ const button = document.querySelector('.button');
 let counter = 0;
 console.log(button);
 
-
-//Añadir un listener al button
-//console.log
-//button.addEventListener('click', handlerButton);
-
-//Funcion generadora de números al azar entre 0-100
 function getRandomNumber(min=0,max=100){
     console.log('chao');
     min = Math.ceil(min);
@@ -41,24 +15,17 @@ function getRandomNumber(min=0,max=100){
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-//console.log
 const randomNumber = getRandomNumber();
 console.log(randomNumber);
 
-
-//Función para que al pulsar el botón de prueba, acceder al contenido del input y mostrarlo en la consola
 function getUserNumber(){
     console.log (userNumber.value);
     return parseInt(userNumber.value);
     
 }
 
-
-//Función comparar el número que el usuario ha escrito en el input con el número aleatorio, y pintar el feedback correspondiente en la pantalla ("demasiado alto", "demasiado bajo", "¡HAS
-//GANADO, CAMPEONA!")
 function handlerButton(event){
     event.preventDefault();
-    console.log('hola');
 
     const userNumberInput = getUserNumber(); 
 
@@ -69,11 +36,10 @@ function handlerButton(event){
     }else if(userNumberInput === randomNumber){
         userResult.innerHTML = 'Has ganado, campeona!';
     }
-//actualizar el contador de intentos cada vez que el usuario pruebe
+
     counter += 1;
     userAttempts.innerHTML = counter;
 }
 
 button.addEventListener('click', handlerButton);
 
-//Poner estilos en CSS
